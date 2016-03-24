b6c2e89192c9088f2876af1d37941a520cba4bb4
Guilherme Cadori <gccadori@hotmail.com>
2016-03-23 23:22:16 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,23 +6,23 @@ m <- NULL
   m <<- NULL
 }
 get <- function() x
-setmatrix <- function(solve) m <<- solve
-getmatrix <- function() m
-list (set=set, get=get,
-      setmatrix=setmatrix,
-      getmatrix=getmatrix)
+setMatrix <- function(solve) m <<- solve
+getMatrix <- function() m
+list (set = set, get = get,
+      setMatrix = setMatrix,
+      getMatrix = getMatrix)
 }
 
 ## Calculates the inverse matrix created with the previous function and
 ## that uses the cached value in case it is necessary
 cacheSolve <- function(x, ...) {
-m <- x$getmatrix()
+m <- x$getMatrix()
     if(!is.null(m)) {
       message("getting cached data")
       return(m)
     }
     matrix <- x$get()
     m <- solve(matrix, ...)
-    x$setmatrix(m)
+    x$setMatrix(m)
     m
 }
